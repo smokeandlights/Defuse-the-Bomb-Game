07b12743544d57ee463ec8eb9c6777c84ce2f636
smokeandlights <smokeandlights@hotmail.com>
2016-11-10 10:29:52 -0500

    Add files via upload

---
+++ BombSquadGame.ino
@@ -0,0 +1,19 @@
+const int wire1=3;
+const int wire2=8;
+const int wire3=13;
+const int safeLED=7;
+const int warningLED=11;
+const int boomLED=2;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(safeLED, OUTPUT);
+pinMode(warningLED, OUTPUT);
+pinMode(boomLED, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
