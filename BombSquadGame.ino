a9d15fc402a06ba410adfd9d49868e4e6292f20e
smokeandlights <smokeandlights@hotmail.com>
2016-11-10 14:56:09 -0500

    Update BombSquadGame.ino

--- BombSquadGame.ino
+++ BombSquadGame.ino
@@ -16,4 +16,27 @@ pinMode(boomLED, OUTPUT);
 void loop() {
   // put your main code here, to run repeatedly:
 
+long randNumber;
+
+void setup(){
+  Serial.begin(9600);
+
+  // if analog input pin 0 is unconnected, random analog
+  // noise will cause the call to randomSeed() to generate
+  // different seed numbers each time the sketch runs.
+  // randomSeed() will then shuffle the random function.
+  randomSeed(analogRead(0));
+}
+
+void loop() {
+  // print a random number from 0 to 299
+  randNumber = random(300);
+  Serial.println(randNumber);  
+
+  // print a random number from 1 to 3
+  randNumber = random(1, 4);
+  Serial.println(randNumber);
+
+  delay(50);
+}
 }
