46f8540be9b6003aab09dd8c750726326c065034
smokeandlights <smokeandlights@hotmail.com>
2016-11-13 16:18:46 -0500

    Update BombSquadGame.ino

--- BombSquadGame.ino
+++ BombSquadGame.ino
@@ -1,63 +1,90 @@
-int winLED = 0;
-int boomLED = 1;
-
 #include "SevSeg.h"
-
 SevSeg sevseg;
 
+int winLED = 1;
+int boomLED = 0;
+int minutes = 1;
+int startTime = ((minutes *600)+1);
+int winner=0;
+int wires[]= {14,15,16};
+  
 void setup() {
-  // put your setup code here, to run once:
-pinMode(winLED, OUTPUT);
-pinMode(boomLED, OUTPUT);
+//    Serial.begin(9600);
+    
+    pinMode(winLED, OUTPUT);
+    pinMode(boomLED, OUTPUT);
+    digitalWrite(winLED,LOW);
+    digitalWrite(boomLED,LOW);
+//    
+    //Countdown Timer stuff
+     
+     byte numDigits = 4;   
+     byte digitPins[] = {2, 3, 4, 5};
+     byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
+    
+     sevseg.begin(COMMON_ANODE, numDigits, digitPins, segmentPins);
+     sevseg.setBrightness(90);
 
-//Countdown Timer stuff
-
- byte numDigits = 4;   
- byte digitPins[] = {2, 3, 4, 5};
- byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
-
-  sevseg.begin(COMMON_ANODE, numDigits, digitPins, segmentPins);
-  sevseg.setBrightness(90);
-
-  //Randomizing the chosen Wire
-  randomSeed(analogRead(A5));//floating analog pin
-  unsigned int randNumber;
-  randNumber = random(0,2);
-  int wires[]= {A0,A1,A2};
-  int winner = wires[randNumber];
-  
-  
-  Serial.begin(9600);
-
-  Serial.print(winner);
-}
-
+     }
+          
 void loop() {
- // if (digitalRead(wires[randNumber])==LOW){
- //digitalWrite(winLED,HIGH);
- //sevseg.setNumber(0, 5);
- //sevseg.refreshDisplay();
-  //}
-
- // else {
-   static unsigned long timer = millis();
-  static int Seconds = 301; 
-if (Seconds>=0){
-  if (millis() >= timer) {
-        Seconds--; // 100 milliSeconds is equal to 1 deciSecond
-    timer += 1000; 
-   // if (deciSeconds == 10000) { // Reset to 0 after counting for 1000 seconds.
-     // deciSeconds=0;
-    //}
-    sevseg.setNumber(Seconds, 5);
-  }
-
-  sevseg.refreshDisplay(); // Must run repeatedly
-
-}
-  else{
- digitalWrite(13,HIGH);
- sevseg.setNumber(0, 5);
- //sevseg.refreshDisplay();
- }   
-  }
+  if(millis()<= 5){
+    int randNumber;
+    randomSeed(analogRead(A5));//floating analog pin
+    randNumber = random(0,3);
+    winner = wires[randNumber];
+            
+//    Serial.print("randNumber: ");
+//    Serial.println(randNumber);
+//    Serial.print("winner: ");
+//    Serial.println(winner);
+          }
+//           if(winner==14){
+//            if (digitalRead(15 )||digitalRead(16)==LOW){
+//              digitalWrite(boomLED, HIGH);
+//              sevseg.setNumber(8888,5);
+//              sevseg.refreshDisplay();
+//            }
+//           }
+//          if(winner==15){
+//            if (digitalRead(14 )||digitalRead(16)==LOW){
+//              digitalWrite(boomLED, HIGH);
+//              sevseg.setNumber(8888,5);
+//              sevseg.refreshDisplay();
+//            }
+//           }
+//           if(winner==16){
+//            if (digitalRead(14 )||digitalRead(15)==LOW){
+//              digitalWrite(boomLED, HIGH);
+//              sevseg.setNumber(8888,5);
+//              sevseg.refreshDisplay();
+//            }
+//           }
+//           
+//  if (digitalRead(wires[winner])==LOW){
+//    digitalWrite(winLED,HIGH);
+//    sevseg.setNumber(0, 5);
+//    sevseg.refreshDisplay();
+//     }
+//    
+// else {
+     static unsigned long timer = millis();
+     static int deciSeconds = startTime; 
+    if(deciSeconds>=0){
+      if (millis() >= timer) {
+        deciSeconds--; // 100 milliSeconds is equal to 1 deciSecond
+        timer += 100; 
+            // if (deciSeconds == 10000) { // Reset to 0 after counting for 1000 seconds.
+            // deciSeconds=0;
+            //}
+        sevseg.setNumber(deciSeconds, 1);
+      }
+      sevseg.refreshDisplay(); // Must run repeatedly
+    }
+    else{
+     digitalWrite(boomLED,HIGH);
+     sevseg.setNumber(8888, 5);
+     sevseg.refreshDisplay();
+     }   
+ }
+//}
