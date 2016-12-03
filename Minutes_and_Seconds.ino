70ba5c3b0e0a0bc93c55faff5078cb0d9f8b2f88
smokeandlights <smokeandlights@hotmail.com>
2016-12-03 07:10:34 -0500

    Add files via upload

---
+++ Minutes_and_Seconds.ino
@@ -0,0 +1,35 @@
+#include <SevenSegmentExtended.h>
+#include <SevenSegmentFun.h>
+#include <SevenSegmentTM1637.h>
+
+int startTime = 5;
+
+
+bool youWIN = false
+              bool youLOSE = false
+
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  if (youWIN == false && youLOSE == false) {
+    for (int minutes = startTime; minutes >= 00; Minutes--) {
+      for (int seconds = 59; seconds >= 00; seconds--) {
+        for (int hundreths = 99; int hundreths >= 00; int hundreths --) {
+          if (minutes >= 1) {
+            Serial.print(minutes);
+            Serial.print(":");
+            Serial.print(seconds);
+          }
+          if (minutes == 0) {
+            Serial.print(seconds);
+            Serial.print(":");
+            Serial.println(hundreths);
+          }
+          else {
+            youLOSE = true;
+            Serial.print("TIMES UP, BOOM!");
+          }
+        }
+      }
+
