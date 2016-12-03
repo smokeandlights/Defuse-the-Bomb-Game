d83c3eb1cc84fce6a257ef6e940a45af9100c426
smokeandlights <smokeandlights@hotmail.com>
2016-12-03 06:43:05 -0500

    Update BombSquadGame.ino

--- BombSquadGame.ino
+++ BombSquadGame.ino
@@ -1,126 +1,179 @@
-#include "SevSeg.h"
-SevSeg sevseg;
+#include <Adafruit_NeoPixel.h>
 
-int winLED = 1;
-int boomLED = 0;
+
+
+#include <SevenSegmentExtended.h>
+#include <SevenSegmentFun.h>
+#include <SevenSegmentTM1637.h>
+
+const byte PIN_CLK = 3;   // define CLK pin (any digital pin)
+const byte PIN_DIO = 2;   // define DIO pin (any digital pin)
+SevenSegmentExtended      countdown(PIN_CLK, PIN_DIO);
+
+
 int minutes = 1;
 int startTime = ((minutes * 600) + 1);
 int winner = 0;
-int wires[] = {14, 15, 16};
+int wires[] = {A0, A1, A2};
 int buzzer = A3;
 int pitch = 100;
-int youWIN = false;
+bool youWIN = false;
+bool youLOSE = false;
+int randNumber = 0;
+int ledPIN = 4;
+int ledoff = 0; //led switch cases
+int ledarmed = 1;
+int ledwin = 2;
+int ledlose = 3;
+unsigned long currentMillis = millis();
+unsigned long previousMillis = 0;
 
 //Countdown Timer stuff, so it's available elswhere
 static unsigned long timer = millis();
 static int deciSeconds = startTime;
 
+Adafruit_NeoPixel led = Adafruit_NeoPixel(1, ledPIN, NEO_RGB + NEO_KHZ800);
+
 void setup() {
-  //    Serial.begin(9600);     //Comment out the LEDs if you uncomment this.
+  Serial.begin(9600);     //Comment out the LEDs if you uncomment this.
 
-  pinMode(winLED, OUTPUT);  //Comment these 4 lines about LEDs out if you want to enable Serial
-  pinMode(boomLED, OUTPUT); //functions . They interfere with each other.
-  digitalWrite(winLED, LOW); //
-  digitalWrite(boomLED, LOW); //
+  countdown.begin();            // initializes the display
+  countdown.setBacklight(100);  // set the brightness to 100 %
+  countdown.setColonOn(true);
+  countdown.setCursor(0, 1);
 
-  //Countdown Timer stuff
+  led.begin();
 
-  byte numDigits = 4;
-  byte digitPins[] = {2, 3, 4, 5};
-  byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
 
-  sevseg.begin(COMMON_ANODE, numDigits, digitPins, segmentPins);
-  sevseg.setBrightness(90);
+  randomSeed(analogRead(A5));//floating analog pin
+  randNumber = random(0, 3);
+  winner = wires[randNumber];
 
+  Serial.print("randNumber: "); //This won't work unless you
+  Serial.println(randNumber);   //comment out the LED functions in setup
+  Serial.print("winner: ");     //AND UNCOMMENT the Serial.begin command.
+  Serial.println(winner);       //
 }
 
 void loop() {
-
-  if (millis() <= 5) {
-    int randNumber;
-    randomSeed(analogRead(A5));//floating analog pin
-    randNumber = random(0, 3);
-    winner = wires[randNumber];
-
-    Serial.print("randNumber: "); //This won't work unless you
-    Serial.println(randNumber);   //comment out the LED functions in setup
-    Serial.print("winner: ");     //AND UNCOMMENT the Serial.begin command.
-    Serial.println(winner);       //
-  }
-  if (winner == 14) {
-    if (digitalRead(15 ) == LOW || digitalRead(16) == LOW) {
-      deciSeconds = 0;
-      digitalWrite(boomLED, HIGH);
-      tone(buzzer, pitch, 500);
-      Serial.print("YOU LOST!"); // replaces LED function when Serial is enabled
-      sevseg.setNumber(8888, 5);
-      sevseg.refreshDisplay();
-    }
-    if (digitalRead(14) == LOW) {
-      digitalWrite(14, LOW);      // Once the wire is cut, it's cut.
-      digitalWrite(winLED, HIGH);
-      youWIN = true;
-      Serial.print("YOU WIN!"); // replaces LED function when Serial is enabled
-      sevseg.setNumber(deciSeconds - 1, 1);
-      sevseg.refreshDisplay();
-    }
-  }
-  if (winner == 15) {
-    if (digitalRead(14 ) == LOW || digitalRead(16) == LOW) {
-      deciSeconds = 0;
-      digitalWrite(boomLED, HIGH);
-      tone(buzzer, pitch, 500);
-      Serial.print("YOU LOST!"); // replaces LED function when Serial is enabled
-      sevseg.setNumber(8888, 5);
-      sevseg.refreshDisplay();
-    }
-    if (digitalRead(15) == LOW) {
-      digitalWrite(15, LOW);      // Once the wire is cut, it's cut.
-      digitalWrite(winLED, HIGH);
-      youWIN = true;
-      Serial.print("YOU WIN!"); // replaces LED function when Serial is enabled
-      sevseg.setNumber(deciSeconds - 1, 1);
-      sevseg.refreshDisplay();
-    }
-  }
-  if (winner == 16) {
-    if (digitalRead(14 ) == LOW || digitalRead(15) == LOW) {
-      deciSeconds = 0;
-      digitalWrite(boomLED, HIGH);
-      tone(buzzer, pitch, 500);
-      Serial.print("YOU LOST!"); // replaces LED function when Serial is enabled
-      sevseg.setNumber(8888, 5);
-      sevseg.refreshDisplay();
-    }
-    if (digitalRead(16) == LOW) {
-      digitalWrite(16, LOW);     // Once the wire is cut, it's cut.
-      digitalWrite(winLED, HIGH);
-      youWIN = true;
-      Serial.print("YOU WIN!"); // replaces LED function when Serial is enabled
-      sevseg.setNumber(deciSeconds - 1, 1);
-      sevseg.refreshDisplay();
-    }
-  }
-  else {
-    if (youWIN == false) {
-      if (deciSeconds >= 0) {
+  smartLED(ledarmed);
+  if (youWIN == false && youLOSE == false) {
 
 
-        if (millis() >= timer) {
+    if (digitalRead(winner) == LOW) {
+      YOUWON();
 
-          deciSeconds--;
-          timer += 100;
-          sevseg.setNumber(deciSeconds, 1);
+    }
+    else {
+      if (winner == A0) {
+        if (digitalRead(A1 ) == LOW || digitalRead(A2) == LOW) {
+          YOULOST();
         }
-        sevseg.refreshDisplay();
       }
-      else {
-        digitalWrite(boomLED, HIGH);
-        tone(buzzer, pitch, 500);
-        sevseg.setNumber(8888, 5);
-        sevseg.refreshDisplay();
+      if (winner == A1) {
+        if (digitalRead(A0 ) == LOW || digitalRead(A2) == LOW) {
+          YOULOST();
+        }
       }
-      sevseg.refreshDisplay();
+      if (winner == A2) {
+        if (digitalRead(A0 ) == LOW || digitalRead(A1) == LOW) {
+          YOULOST();
+        }
+      }
+    }
+
+
+    if (deciSeconds >= 1) {
+      if (youWIN == false && youLOSE == false) {
+        if (deciSeconds != 8888) {
+          if (millis() >= timer && deciSeconds >= 0) {
+            deciSeconds--;
+            timer += 100;
+
+            countdown.setCursor(0, 0);
+            countdown.print(deciSeconds);
+          }
+        }
+      }
+    }
+    else {
+      YOULOST();
     }
   }
+}
+
+void winTUNE() {//mario 1-up
+  tone(buzzer, 659, 120);//E5
+  tone(buzzer, (659 / 2), 120);
+  delay(120);//try to eliminate the delays and extend the notes - which sounds better?
+  tone(buzzer, 784, 120);//G5
+  delay(130);
+  tone(buzzer, 1397, 120);//F6
+  delay(130);
+  tone(buzzer, 1175, 120);//D6
+  delay(130);
+  tone(buzzer, 1319, 120);//E6
+  delay(130);
+  tone(buzzer, 1760, 120);//A6
+}
+
+void YOUWON() {
+  digitalWrite(winner, LOW);     // Once the wire is cut, it's cut.
+  smartLED(ledwin);
+  youWIN = true;
+  Serial.print("YOU WIN!");
+  countdown.print(deciSeconds - 1);
+  winTUNE();
+}
+
+void YOULOST() {
+  youLOSE = true;
+  smartLED(ledlose);
+  Serial.print("YOU LOST!");
+  countdown.setColonOn(false);
+  countdown.print("LOSE");
+  tone(buzzer, pitch, 2000);
+
+}
+void smartLED(int i) {
+  //"neopixel" led showing different colors & patterns for different conditions
+  int ledState = (0, 0, 0);
+  unsigned long currentMillis = millis();
+  unsigned long previousMillis = currentMillis;
+  unsigned int rate = 500;  //armed blink rate is 2Hz
+  if (deciSeconds <= 300) {     //armed blink rate under 30 seconds
+    rate = 250;                 //is 4Hz
+  }
+
+  switch (i) {
+    case 0:
+      // off: dark
+      led.setPixelColor(0, (0, 0, 0));
+      led.show();
+
+    case 1:
+
+      // armed: blinking yellow
+
+      if (currentMillis - previousMillis >= rate) {
+        if (ledState == (0, 0, 0)) {
+          ledState = (127, 127, 0);
+        } else {
+          ledState = (0, 0, 0);
+        }
+      }
+      led.setPixelColor(0, ledState);
+      led.show();
+
+    case 2:
+      // win: full green
+      led.setPixelColor(0, (0, 255, 0));
+      led.show();
+
+    case 3:
+      // lose:full red
+      led.setPixelColor(0, (255, 0, 0));
+      led.show();
+  }
+
 }
