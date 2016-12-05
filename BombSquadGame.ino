946957768f86b2dc377bd3c7438b7b036f2ac845
smokeandlights <smokeandlights@hotmail.com>
2016-12-04 20:57:02 -0500

    Update BombSquadGame.ino

--- BombSquadGame.ino
+++ BombSquadGame.ino
@@ -83,19 +83,48 @@ void loop() {
     }
 
 
-    if (deciSeconds >= 1) {
-      if (youWIN == false && youLOSE == false) {
-        if (deciSeconds != 8888) {
-          if (millis() >= timer && deciSeconds >= 0) {
-            deciSeconds--;
-            timer += 100;
+   void loop() {
+  if (youWIN == false && youLOSE == false) {
+    for (int i = 1; i > 0; i--) {
+      for (int minutes = (startTime - 1); minutes >= 00; minutes--) {
+        for (int seconds = 59; seconds >= 00; seconds--) {
+          int currentMillis = millis();
+          if (currentMillis >= (previousMillis + 10)) {
+            previousMillis = currentMillis;
+            currentMillis = millis();
+            for (int hundreths = 99; hundreths >= 00; hundreths --) {
 
-            countdown.setCursor(0, 0);
-            countdown.print(deciSeconds);
+              if (minutes >= 1) {
+                Serial.print(minutes);
+                Serial.print(":");
+                Serial.println(seconds);
+              }
+              if (minutes == 0) {
+                if (hundreths == 0) {
+                  youLOSE = true;
+                  Serial.print("TIMES UP, BOOM!");
+                  break;
+                }
+
+                if (hundreths <= 9) {
+                  Serial.print(seconds);
+                  Serial.print(":0");
+                  Serial.println(hundreths);
+                }
+                else {
+                  Serial.print(seconds);
+                  Serial.print(":");
+                  Serial.println(hundreths);
+                }
+              }
+            }
           }
         }
       }
     }
+  }
+}
+
     else {
       YOULOST();
     }
