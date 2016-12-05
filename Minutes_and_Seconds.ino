c98e8fc7e9f66ea34fe5b520af84ebb40084035e
smokeandlights <smokeandlights@hotmail.com>
2016-12-04 20:34:31 -0500

    Update Minutes_and_Seconds.ino

--- Minutes_and_Seconds.ino
+++ Minutes_and_Seconds.ino
@@ -1,35 +1,54 @@
-#include <SevenSegmentExtended.h>
-#include <SevenSegmentFun.h>
-#include <SevenSegmentTM1637.h>
 
-int startTime = 5;
+int startTime = 1;
 
 
-bool youWIN = false
-              bool youLOSE = false
+bool youWIN = false;
+bool youLOSE = false;
+
+int previousMillis = 0;
 
 void setup() {
   Serial.begin(9600);
+
 }
 void loop() {
   if (youWIN == false && youLOSE == false) {
-    for (int minutes = startTime; minutes >= 00; Minutes--) {
-      for (int seconds = 59; seconds >= 00; seconds--) {
-        for (int hundreths = 99; int hundreths >= 00; int hundreths --) {
-          if (minutes >= 1) {
-            Serial.println(minutes);
-            Serial.print(":");
-            Serial.print(seconds);
-          }
-          if (minutes == 0) {
-            Serial.println(seconds);
-            Serial.print(":");
-            Serial.print(hundreths);
-          }
-          else {
-            youLOSE = true;
-            Serial.print("TIMES UP, BOOM!");
+    for (int i = 1; i > 0; i--) {
+      for (int minutes = (startTime - 1); minutes >= 00; minutes--) {
+        for (int seconds = 59; seconds >= 00; seconds--) {
+          int currentMillis = millis();
+          if (currentMillis >= (previousMillis + 10)) {
+            previousMillis = currentMillis;
+            currentMillis = millis();
+            for (int hundreths = 99; hundreths >= 00; hundreths --) {
+
+              if (minutes >= 1) {
+                Serial.print(minutes);
+                Serial.print(":");
+                Serial.println(seconds);
+              }
+              if (minutes == 0) {
+                if (hundreths == 0) {
+                  youLOSE = true;
+                  Serial.print("TIMES UP, BOOM!");
+                  break;
+                }
+
+                if (hundreths <= 9) {
+                  Serial.print(seconds);
+                  Serial.print(":0");
+                  Serial.println(hundreths);
+                }
+                else {
+                  Serial.print(seconds);
+                  Serial.print(":");
+                  Serial.println(hundreths);
+                }
+              }
+            }
           }
         }
       }
-
+    }
+  }
+}
